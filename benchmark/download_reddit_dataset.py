8c1471f02d2185a316c85a2684fe2e44fb836bae
Huyuwei <huyuwei1995@gmail.com>
2020-12-02 11:56:27 -0500

    make coo matrix sorted to be consistent with the paper

--- benchmark/download_reddit_dataset.py
+++ benchmark/download_reddit_dataset.py
@@ -9,9 +9,10 @@ if not os.path.isdir('data'):
 data = RedditDataset()
 
 adj_scipy_csr = data.graph.adjacency_matrix_scipy(fmt='csr')
+assert adj_scipy_csr.has_canonical_format  # the matrix has sorted indices and no duplicates
 adj_scipy_csr.data = adj_scipy_csr.data.astype('float32')
 scipy.sparse.save_npz('data/reddit_csr_float32.npz', adj_scipy_csr)
 
-adj_scipy_coo = data.graph.adjacency_matrix_scipy(fmt='coo')
-adj_scipy_coo.data = adj_scipy_coo.data.astype('float32')
+adj_scipy_coo = adj_scipy_csr.tocoo()
+assert adj_scipy_csr.has_canonical_format  # the matrix has sorted indices and no duplicates
 scipy.sparse.save_npz('data/reddit_coo_float32.npz', adj_scipy_coo)
