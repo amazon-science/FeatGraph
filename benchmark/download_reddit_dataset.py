d9f6db80c3ac8d283c444267298dff3b6d027369
Huyuwei <huyuwei1995@gmail.com>
2021-04-06 00:34:48 -0400

    ensure that the sparse matrix has a canonical format

--- benchmark/download_reddit_dataset.py
+++ benchmark/download_reddit_dataset.py
@@ -9,6 +9,8 @@ if not os.path.isdir('data'):
 data = RedditDataset()
 
 adj_scipy_csr = data.graph.adjacency_matrix_scipy(fmt='csr')
+adj_scipy_csr.sort_indices()
+adj_scipy_csr.sum_duplicates()
 assert adj_scipy_csr.has_canonical_format  # the matrix has sorted indices and no duplicates
 adj_scipy_csr.data = adj_scipy_csr.data.astype('float32')
 scipy.sparse.save_npz('data/reddit_csr_float32.npz', adj_scipy_csr)
